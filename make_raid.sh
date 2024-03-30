fb8ea972798672e4ae20c87adc3b36cb87bb5dce
DemBeshtau <149678567+DemBeshtau@users.noreply.github.com>
2024-03-30 21:27:49 +0300

    Add files via upload

---
+++ make_raid.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo -i
+mdadm --create --verbose /dev/md0 -l 10 -n 4 /dev/sd{b,c,d,e}
+mkdir /etc/mdadm
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
+ 
